5272626663dfa6bd60255a7f813e6fefd4008906
Marcello Pogliani <pogliamarci@hotmail.it>
2012-12-27 17:13:30 +0100

    Added equals() and hashCode() on Friendship and User

--- swimv2-ejb/src/main/java/it/polimi/swimv2/entity/User.java
+++ swimv2-ejb/src/main/java/it/polimi/swimv2/entity/User.java
@@ -97,5 +97,26 @@ public class User implements Serializable {
 	public void setDescription(String description) {
 		this.description = description;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (!(obj instanceof User))
+			return false;
+		User other = (User) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
    
 }
