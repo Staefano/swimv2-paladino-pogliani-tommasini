ee382e7fcecbbb4b79e50befcb762793cae995ef
Stefano <staefano@gmail.com>
2012-12-27 16:46:23 +0100

    entities

---
+++ swimv2-ejb/src/main/java/it/polimi/swimv2/entity/Friendship.java
@@ -0,0 +1,46 @@
+package it.polimi.swimv2.entity;
+
+import it.polimi.swimv2.entity.User;
+import java.io.Serializable;
+import javax.persistence.*;
+
+/**
+ * Entity implementation class for Entity: Friendship
+ *
+ */
+@Entity @IdClass(FriendshipId.class)
+
+public class Friendship implements Serializable {
+	
+	@Id	private User user1;
+	@Id	private User user2;
+	
+	private boolean is_direct;
+	private static final long serialVersionUID = 1L;
+
+	public Friendship() {
+		super();
+	}   
+	public User getUser1() {
+		return this.user1;
+	}
+
+	public void setUser1(User user1) {
+		this.user1 = user1;
+	}   
+	public User getUser2() {
+		return this.user2;
+	}
+
+	public void setUser2(User user2) {
+		this.user2 = user2;
+	}   
+	public boolean getIs_direct() {
+		return this.is_direct;
+	}
+
+	public void setIs_direct(boolean is_direct) {
+		this.is_direct = is_direct;
+	}
+   
+}
---
+++ swimv2-ejb/src/main/java/it/polimi/swimv2/entity/FriendshipId.java
@@ -0,0 +1,8 @@
+package it.polimi.swimv2.entity;
+
+public class FriendshipId {
+	
+	private User user1;
+	private User user2;
+
+}
