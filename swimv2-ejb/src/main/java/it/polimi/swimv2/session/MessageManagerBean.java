9665c55f11c419d26fa5fcc077c86118298ddaf2
Marcello Pogliani <marcello.pogliani@gmail.com>
2013-01-04 18:05:32 +0100

    MessageManager / p.2

--- swimv2-ejb/src/main/java/it/polimi/swimv2/session/MessageManagerBean.java
+++ swimv2-ejb/src/main/java/it/polimi/swimv2/session/MessageManagerBean.java
@@ -7,22 +7,36 @@ import java.util.List;
 
 import javax.ejb.Stateless;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
 @Stateless
 public class MessageManagerBean implements MessageManagerBeanRemote {
+
+	@PersistenceContext(unitName="swimv2")
+	private EntityManager manager;
 	
 	public void write(User from, User to, String text) {
-		
+		Message m = new Message(from, to, text);
+		manager.persist(m);
 	}
 	
 	// all the messages or with a limitation???
+	@SuppressWarnings("unchecked")
 	public List<Message> getByUsername(User from, User to) {
-		return null;
+		final String Q = "from Message m " +
+				"where m.sender = :from and m.receiver = :to " +
+				"order by m.timestamp desc";
+		Query q = manager.createQuery(Q);
+		q.setParameter("from",  from);
+		q.setParameter("to", to);
+		return (List<Message>) q.getResultList();
 	}
 	
-	// latest what???
+	// TODO non teniamo traccia degli unread!!!
 	public List<Message> getLatestUnread(User to) {
 		return null;
 	}
-	
 
 }
