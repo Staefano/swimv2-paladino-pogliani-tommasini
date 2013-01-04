6db6b0a8dad97df483de3fe72f327735f238a00c
riccardo <tomma156@gmail.com>
2013-01-04 19:35:44 +0100

    userbean e interfaccia

---
+++ swimv2-ejb/src/main/java/it/polimi/swimv2/session/UserBeanRemote.java
@@ -0,0 +1,27 @@
+package it.polimi.swimv2.session;
+
+import java.util.List;
+
+import it.polimi.swimv2.entity.HelpRequest;
+import it.polimi.swimv2.entity.Notification;
+import it.polimi.swimv2.entity.User;
+import it.polimi.swimv2.entity.Feedback;
+import it.polimi.swimv2.session.exceptions.NoSuchUserException;
+
+import javax.ejb.Remote;
+
+@Remote
+public interface UserBeanRemote {
+
+	User editProfile(User u) throws NoSuchUserException;
+
+	List<Feedback> getHelperFeedbacks(User u) throws NoSuchUserException;
+
+	List<Feedback> getAskerFeedbacks(User u) throws NoSuchUserException;
+
+	List<Notification> getNotifications(User u) throws NoSuchUserException;;
+
+	List<HelpRequest> getOpenedHelpRequest(User u) throws NoSuchUserException;;
+
+	List<HelpRequest> getClosedHelpRequest(User u) throws NoSuchUserException;;
+}
