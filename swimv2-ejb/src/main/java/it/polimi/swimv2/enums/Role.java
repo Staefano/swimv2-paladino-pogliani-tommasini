e83afe270bae45be039c4d7ea0a8dc734607e35f
Marcello Pogliani <pogliamarci@hotmail.it>
2013-01-11 23:10:15 +0100

    Rimosse variabili\import non usati, ...

--- swimv2-ejb/src/main/java/it/polimi/swimv2/enums/Role.java
+++ swimv2-ejb/src/main/java/it/polimi/swimv2/enums/Role.java
@@ -2,12 +2,6 @@ package it.polimi.swimv2.enums;
 
 public enum Role {
 
-	HELPER("helper"), ASKER("asker");
-	
-	private String str;
-	private Role(String s) {
-
-		this.str = s;
-	}
+	HELPER, ASKER;
 
 }
--- swimv2-ejb/src/main/java/it/polimi/swimv2/session/exceptions/NoResultFoundException.java
+++ swimv2-ejb/src/main/java/it/polimi/swimv2/session/exceptions/NoResultFoundException.java
@@ -2,4 +2,6 @@ package it.polimi.swimv2.session.exceptions;
 
 public class NoResultFoundException extends Exception {
 
+	private static final long serialVersionUID = 2754518591432486291L;
+
 }
